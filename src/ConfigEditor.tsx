bfc84668163f0d0b53eaa3d49ca488a298cc31ee
Jack Westbrook <jack.westbrook@gmail.com>
2023-01-27 17:37:12 +0100

    feat(frontend): clean up configeditor

--- src/ConfigEditor.tsx
+++ src/ConfigEditor.tsx
@@ -1,9 +1,9 @@
 import React, { ChangeEvent, PureComponent } from 'react';
-import { LegacyForms } from '@grafana/ui';
+import { InlineField, LegacyForms } from '@grafana/ui';
 import { DataSourcePluginOptionsEditorProps } from '@grafana/data';
 import { MyDataSourceOptions, MySecureJsonData } from './types';
 
-const { SecretFormField, FormField } = LegacyForms;
+const { SecretFormField } = LegacyForms;
 
 interface Props extends DataSourcePluginOptionsEditorProps<MyDataSourceOptions> {}
 
@@ -20,61 +20,53 @@ export class ConfigEditor extends PureComponent<Props, State> {
   };
 
   // Secure field (only sent to the backend)
-  onAPIKeyChange = (event: ChangeEvent<HTMLInputElement>) => {
+  onAPITokenChange = (event: ChangeEvent<HTMLInputElement>) => {
     const { onOptionsChange, options } = this.props;
     onOptionsChange({
       ...options,
       secureJsonData: {
-        apiKey: event.target.value,
+        apiToken: event.target.value,
       },
     });
   };
 
-  onResetAPIKey = () => {
+  onResetAPIToken = () => {
     const { onOptionsChange, options } = this.props;
     onOptionsChange({
       ...options,
       secureJsonFields: {
         ...options.secureJsonFields,
-        apiKey: false,
+        apiToken: false,
       },
       secureJsonData: {
         ...options.secureJsonData,
-        apiKey: '',
+        apiToken: '',
       },
     });
   };
 
   render() {
     const { options } = this.props;
-    const { jsonData, secureJsonFields } = options;
+    const { secureJsonFields } = options;
     const secureJsonData = (options.secureJsonData || {}) as MySecureJsonData;
 
     return (
       <div className="gf-form-group">
-        <div className="gf-form">
-          <FormField
-            label="Path"
-            labelWidth={6}
-            inputWidth={20}
-            onChange={this.onPathChange}
-            value={jsonData.path || ''}
-            placeholder="json field returned to frontend"
-          />
-        </div>
-
-        <div className="gf-form-inline">
-          <div className="gf-form">
+        <InlineField>
             <SecretFormField
-              isConfigured={(secureJsonFields && secureJsonFields.apiKey) as boolean}
-              value={secureJsonData.apiKey || ''}
-              label="API Key"
+              isConfigured={(secureJsonFields && secureJsonFields.apiToken) as boolean}
+              value={secureJsonData.apiToken || ''}
+              label="API Token"
               placeholder="secure json field (backend only)"
               labelWidth={6}
               inputWidth={20}
-              onReset={this.onResetAPIKey}
-              onChange={this.onAPIKeyChange}
+              onReset={this.onResetAPIToken}
+              onChange={this.onAPITokenChange}
             />
+
+        </InlineField>
+        <div className="gf-form-inline">
+          <div className="gf-form">
           </div>
         </div>
       </div>
--- src/datasource.ts
+++ src/datasource.ts
@@ -1,17 +1,14 @@
-// import defaults from 'lodash/defaults';
 import { Observable, of } from 'rxjs';
-import {
-  DataQueryRequest,
-  DataQueryResponse,
-  // DataQueryRequest,
-  // DataSourceApi,
-  DataSourceInstanceSettings,
-  getDataSourceRef,
-  // MutableDataFrame,
-  // FieldType,
-} from '@grafana/data';
+import { DataQueryRequest, DataQueryResponse, DataSourceInstanceSettings, getDataSourceRef } from '@grafana/data';
 
-import { BackendDataSourceResponse, DataSourceWithBackend, getBackendSrv, getDataSourceSrv } from '@grafana/runtime';
+import {
+  BackendDataSourceResponse,
+  DataSourceWithBackend,
+  getBackendSrv,
+  getDataSourceSrv,
+  HealthCheckError,
+  HealthStatus,
+} from '@grafana/runtime';
 
 import { MyQuery, MyDataSourceOptions } from './types';
 
@@ -85,115 +82,17 @@ export class DataSource extends DataSourceWithBackend<MyQuery, MyDataSourceOptio
 
     return of({ data: [] });
   }
-  /**
-   * Ideally final -- any other implementation may not work as expected
-   */
-  // query(request: DataQueryRequest<TQuery>): Observable<DataQueryResponse> {
-  //   const { intervalMs, maxDataPoints, range, requestId } = request;
-  //   let targets = request.targets;
 
-  //   if (this.filterQuery) {
-  //     targets = targets.filter((q) => this.filterQuery!(q));
-  //   }
+  async testDatasource(): Promise<any> {
+    return this.callHealthCheck().then((res) => {
+      if (res.status === HealthStatus.OK) {
+        return {
+          status: 'success',
+          message: res.message,
+        };
+      }
 
-  //   const queries = targets.map((q) => {
-  //     let datasource = this.getRef();
-  //     let datasourceId = this.id;
-
-  //     if (isExpressionReference(q.datasource)) {
-  //       return {
-  //         ...q,
-  //         datasource: ExpressionDatasourceRef,
-  //       };
-  //     }
-
-  //     if (q.datasource) {
-  //       const ds = getDataSourceSrv().getInstanceSettings(q.datasource, request.scopedVars);
-
-  //       if (!ds) {
-  //         throw new Error(`Unknown Datasource: ${JSON.stringify(q.datasource)}`);
-  //       }
-
-  //       datasource = ds.rawRef ?? getDataSourceRef(ds);
-  //       datasourceId = ds.id;
-  //     }
-
-  // return {
-  //   ...this.applyTemplateVariables(q, request.scopedVars),
-  //   datasource,
-  //   datasourceId, // deprecated!
-  //   intervalMs,
-  //   maxDataPoints,
-  // };
-  //   });
-
-  //   // Return early if no queries exist
-  //   if (!queries.length) {
-  //     return of({ data: [] });
-  //   }
-
-  //   const body: any = { queries };
-
-  //   if (range) {
-  //     body.range = range;
-  //     body.from = range.from.valueOf().toString();
-  //     body.to = range.to.valueOf().toString();
-  //   }
-
-  //   if (config.featureToggles.queryOverLive) {
-  //     return getGrafanaLiveSrv().getQueryData({
-  //       request,
-  //       body,
-  //     });
-  //   }
-
-  //   return getBackendSrv()
-  //     .fetch<BackendDataSourceResponse>({
-  //       url: '/api/ds/query',
-  //       method: 'POST',
-  //       data: body,
-  //       requestId,
-  //     })
-  //     .pipe(
-  //       switchMap((raw) => {
-  //         const rsp = toDataQueryResponse(raw, queries as DataQuery[]);
-  //         // Check if any response should subscribe to a live stream
-  //         if (rsp.data?.length && rsp.data.find((f: DataFrame) => f.meta?.channel)) {
-  //           return toStreamingDataResponse(rsp, request, this.streamOptionsProvider);
-  //         }
-  //         return of(rsp);
-  //       }),
-  //       catchError((err) => {
-  //         return of(toDataQueryResponse(err));
-  //       })
-  //     );
-  // }
-
-  // async query(options: DataQueryRequest<MyQuery>): Promise<DataQueryResponse> {
-  //   const { range } = options;
-  //   const from = range!.from.valueOf();
-  //   const to = range!.to.valueOf();
-
-  //   // Return a constant for each query.
-  //   const data = options.targets.map((target) => {
-  //     const query = defaults(target, defaultQuery);
-  //     return new MutableDataFrame({
-  //       refId: query.refId,
-  //       fields: [
-  //         { name: 'Time', values: [from, to], type: FieldType.time },
-  //         { name: 'Value', values: [query.constant, query.constant], type: FieldType.number },
-  //       ],
-  //     });
-  //   });
-
-  //   return { data };
-  // }
-
-  async testDatasource() {
-    // Implement a health check for your data source.
-    return {
-      status: 'success',
-      message: 'Success',
-    };
+      throw new HealthCheckError(res.message, res.details);
+    });
   }
 }
